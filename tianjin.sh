b5dae0f67b41eb9abc9a660d13ee3feaf2acb145
beikehanbao23 <369103724@qq.com>
2018-05-23 17:56:37 +0800

    [script] add quick generate script

---
+++ tianjin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo " "
+echo "-------------------------------------------------------------------------------------"
+echo "====> : Remove old generated source files ( ./build/* ) ( if they exist ) "
+docker-compose run --rm openmaptiles-tools make clean
+
+echo " "
+echo "-------------------------------------------------------------------------------------"
+echo "====> : Code generating from the layer definitions ( ./build/mapping.yaml; ./build/tileset.sql )"
+echo "      : The tool source code: https://github.com/openmaptiles/openmaptiles-tools "
+echo "      : But we generate the tm2source, Imposm mappings and SQL functions from the layer definitions! "
+docker-compose run --rm openmaptiles-tools make
+
+echo " "
+echo "-------------------------------------------------------------------------------------"
+echo "====> : Start generating MBTiles (containing gzipped MVT PBF) from a TM2Source project. "
+echo "      : TM2Source project definitions : ./build/openmaptiles.tm2source/data.yml "
+echo "      : Output MBTiles: ./data/tiles.mbtiles  "
+echo "      : Source code: https://github.com/openmaptiles/generate-vectortiles "
+echo "      : We are using a lot of Mapbox Open Source tools! : https://github.com/mapbox "
+echo "      : Thank you https://www.mapbox.com !"
+echo "      : See other MVT tools : https://github.com/mapbox/awesome-vector-tiles "
+echo "      :  "
+echo "      : You will see a lot of deprecated warning in the log! This is normal!  "
+echo "      :    like :  Mapnik LOG>  ... is deprecated and will be removed in Mapnik 4.x ... "
+
+docker-compose -f docker-compose.yml -f ./data/docker-compose-config.yml  run --rm generate-vectortiles
+
+echo " "
+echo "-------------------------------------------------------------------------------------"
+echo "====> : Add special metadata to mbtiles! "
+docker-compose run --rm openmaptiles-tools  generate-metadata ./data/tiles.mbtiles
+docker-compose run --rm openmaptiles-tools  chmod 666         ./data/tiles.mbtiles
