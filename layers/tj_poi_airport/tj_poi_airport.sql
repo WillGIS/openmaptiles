d7b453e092d07a3ddfd9236383ba278c68af805a
beikehanbao23 <369103724@qq.com>
2018-07-15 17:52:02 +0800

    [poi]poi输出字段修改

--- layers/tj_poi_airport/tj_poi_airport.sql
+++ layers/tj_poi_airport/tj_poi_airport.sql
@@ -1,7 +1,7 @@
 
 CREATE OR REPLACE FUNCTION layer_tj_poi_airport (bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, typecode text, name text, objectid int) AS $$
-    SELECT geometry, typecode, name, objectid::int FROM (
+RETURNS TABLE(geometry geometry, typecode text, name text, objectid int, level_f int, level_t int) AS $$
+    SELECT geometry, typecode, name, objectid::int, level_f::int, level_t::int FROM (
         -- etldoc: fangwujianzhu -> layer_tjbuilding:z13
         SELECT * FROM tj_poi_airport WHERE geometry && bbox AND  zoom_level < 14 AND zoom_level >= level_f AND zoom_level <= level_t 
         UNION ALL
