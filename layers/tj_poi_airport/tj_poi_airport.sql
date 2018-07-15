7dee4604a5d79cacc76bdcc8159218d502a66f7f
beikehanbao23 <369103724@qq.com>
2018-07-15 12:37:44 +0800

    [poi]所有POI进入到第14级切片

--- layers/tj_poi_airport/tj_poi_airport.sql
+++ layers/tj_poi_airport/tj_poi_airport.sql
@@ -3,6 +3,9 @@ CREATE OR REPLACE FUNCTION layer_tj_poi_airport (bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, typecode text, name text, objectid int) AS $$
     SELECT geometry, typecode, name, objectid::int FROM (
         -- etldoc: fangwujianzhu -> layer_tjbuilding:z13
-        SELECT * FROM tj_poi_airport WHERE geometry && bbox AND zoom_level >= level_f AND zoom_level <= level_t
+        SELECT * FROM tj_poi_airport WHERE geometry && bbox AND  zoom_level < 14 AND zoom_level >= level_f AND zoom_level <= level_t 
+        UNION ALL
+        SELECT * FROM tj_poi_airport WHERE geometry && bbox AND zoom_level = 14
+
     ) AS zoom_levels;
 $$ LANGUAGE SQL IMMUTABLE;
