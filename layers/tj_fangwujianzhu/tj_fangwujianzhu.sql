fed6513e038da9cf78a359784c9e0c281bd7ec40
huzhenhui <369103724@qq.com>
2018-10-10 20:22:01 +0800

    fix：建筑高度不取整数

--- layers/tj_fangwujianzhu/tj_fangwujianzhu.sql
+++ layers/tj_fangwujianzhu/tj_fangwujianzhu.sql
@@ -15,12 +15,12 @@
 -- etldoc:     label="<sql> layer_tj_building |<z0> z0 |<z1_2> z1_2 | <z3> z3 | <z4> z4 | <z5> z5 | <z6> z6 | <z7> z7 | <z8> z8 | <z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13+"]
 
 CREATE OR REPLACE FUNCTION layer_tj_fangwujianzhu (bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, render_height int, render_min_height int) AS $$
-    SELECT  geometry,  render_height::int, render_min_height::int FROM (
+RETURNS TABLE(geometry geometry, render_height int, render_min_height int, query_height numeric) AS $$
+    SELECT  geometry,  render_height::int, render_min_height::int, query_height::numeric FROM (
         -- etldoc: fangwujianzhu ->  layer_tj_building:z13
-        SELECT   geometry, fw_g as render_height, gc as render_min_height FROM tj_fangwu_fangwujianzhu WHERE geometry && bbox AND zoom_level = 13
+        SELECT   geometry, fw_g as render_height, fw_g as query_height, gc as render_min_height FROM tj_fangwu_fangwujianzhu WHERE geometry && bbox AND zoom_level = 13
         UNION ALL
         -- etldoc: fangwujianzhu -> layer_tj_building:z14
-        SELECT   geometry, fw_g as render_height, gc as render_min_height FROM tj_fangwu_fangwujianzhu WHERE geometry && bbox AND zoom_level >= 14
+        SELECT   geometry, fw_g as render_height, fw_g as query_height, gc as render_min_height FROM tj_fangwu_fangwujianzhu WHERE geometry && bbox AND zoom_level >= 14
     ) AS zoom_levels;
 $$ LANGUAGE SQL IMMUTABLE;
